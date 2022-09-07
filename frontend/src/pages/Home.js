f5a199bebb4841a4eaffc3d2eea8b2d007c05a65
SentiCryptdev <senticrypt@gmail.com>
2022-09-07 14:03:23 +0300

    Adding Data through Form Works

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+import NavBar from "../components/NavBar";
+
+const Home = () => {
+    return ( 
+        <div className="Home">
+            <h3>Home</h3> 
+        </div>
+     );
+}
+ 
+export default Home;
